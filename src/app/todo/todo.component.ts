4701b0189c49959400540ddd736484a6cd948781
Asia Watts <wattsasiak@gmail.com>
2021-01-06 22:33:47 -0500

    adding most recent changes

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Todo{
+  task: string;
+  completed: boolean;
+}
+
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  public Todos: Todo[];
+ 
+  // input: string = "Add your to-do";
+  
+  constructor() {
 
-  constructor() { }
+    this.Todos = [
+      {task: 'fold clothes', completed: true},
+      {task: 'put clothes in dresser', completed: true},
+      {task: 'relax', completed: true},
+      {task: 'try to pet cat', completed: false},
+      {task: 'pet dog', completed: true},
+      {task: 'be awesome', completed: true},
+    ];
+   }
 
   ngOnInit(): void {
   }
